77d80a9c9778172d2813be1e11b91b2d90c86100
abinonayq <abinonayq@elm.sa>
2019-03-13 15:18:08 +0300

    a

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-$(function(){ //must always be here if you use JQuery
+$(function ready(doc){ //must always be here if you use JQuery
     
-    let db = firebase.firestore().collection('restaurants')
+    let db = firebase.firestore().collection('resturants')
     let resList = $('.res-container')
 
-    db.get()
-    .then(result => {
-        let changes = result.docChanges() //gets array of docs
+    var tdate = new Date();
+    var dd = tdate.getDate(); 
+    var MM = ((tdate.getMonth().length+1) === 1)? (tdate.getMonth()+1) : '0' + (tdate.getMonth()+1);
+    var yyyy = tdate.getFullYear();
+    var currentDate= yyyy + "-" + MM + "-" + dd;
 
+    console.log(currentDate)
+
+    db.where('date','==',currentDate).get().then(result => {
+        let changes = result.docChanges() //gets array of docs
+        console.log(changes)
         changes.forEach(res => {
             console.log(res.doc.data());
-            resList.append(`<li data-id="${res.doc.id}">${res.doc.data().name} - ${res.doc.data().location} <button class="edit">edit</button> <button class="delete">delete</button> </li>`)
+            resList.append(`<li data-id="${res.doc.id}">${res.doc.data().name} - ${res.doc.data().location} - ${res.doc.data().date} <button class="edit">edit</button> <button class="delete">delete</button> </li>`)            
+        });        
+    }).catch(err => console.log(err))
+
+
+ db.onSnapshot( result => {
+        let changes = result.docChanges(); //gets array of docs
+        changes.forEach(res => {
+            // console.log(res.doc.data());
+            if(res.type == 'added' ) {
+                console.log(res.doc.data());
+                // ready(res.doc)
+            }else if (res.type == 'removed'){
+                let li = resList.$('[date-id=' + res.doc.id + ']')
+                resList.removeChild(li)
+            }
+            // resList.append(`<li data-id="${res.doc.id}">${res.doc.data().name} - ${res.doc.data().location} - ${res.doc.data().date} <button class="edit">edit</button> <button class="delete">delete</button> </li>`)
 
             
         });
         
-    }).catch(err => console.log(err))
+    })
 
 
     resList.on('click', ".delete", function(){
-        // $(this).parent().attr("data-id")
-        let id = $(this).parent().data("id")
+         //$(this).parent().attr("data-id")
+         let id =$(this).parent().data("id")
 
         db.doc(id).delete()
         // .then()
+
 
     })
 
@@ -48,23 +72,29 @@ $(function(){ //must always be here if you use JQuery
 
     //create data and store to restaurants collection
     $('.submit').click(function(){
-        
-        let name = $('input[name=name]').val()
-        let location = $('input[name=location]').val()
+                
+        let nameI = $('input[name=name]').val()
+        let locationI = $('input[name=location]').val()
+        let dateI = $('input[name=bday]').val()
 
-        console.log(name)
-        console.log(location)
+     
 
         db.add({
-            name: name,
-            location: location
+            name: nameI,
+            location: locationI,
+            date: dateI
         }).then(res =>{
-            resList.append(`<li data-id="${res.id}">${name} - ${location} <button class="edit">edit</button> <button class="delete">delete</button></li>`)
+
+            console.log(` ${name} - ${locationI}` );
             
-            
-        })
+            resList.append(`<li data-id="${res.id}">${nameI} - ${locationI} - ${dateI} <button class="edit">edit</button> <button class="delete">delete</button></li>`)     
+                   })     
+
+                   $('input[name=name]').val('')
+                   $('input[name=location]').val('')
+                   $('input[name=bday]').val('')
         
-        
+  
     })
 
 })
